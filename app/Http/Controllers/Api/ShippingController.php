68c5e3b85524551b86906dadfd68038ebe55b81f
jardiano <jardiano.almeida@gmail.com>
2020-05-23 16:40:09 -0300

    add shipping address

--- app/Http/Controllers/Api/ShippingController.php
+++ app/Http/Controllers/Api/ShippingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Shipping;
 use Illuminate\Http\Request;
 
 class ShippingController extends Controller
@@ -35,7 +36,14 @@ class ShippingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $shipping = new Shipping();
+        $shipping->name = $request->input('name');
+        $shipping->email = $request->input('email');
+        $shipping->address = $request->input('address');
+        if($shipping->save()){
+            return response(['result' => true]);
+        }
+        return response(['result' => false]);
     }
 
     /**
